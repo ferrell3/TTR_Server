2c1153b60267633273d5f4478dfa5d78d3bb8707
Jordan Ferrell <jordanferrell@byu.edu>
2018-02-15 18:32:08 -0700

    Update gameServices - print error messages and fix Integer in poller

--- server/src/main/java/Services/GameServices.java
+++ server/src/main/java/Services/GameServices.java
@@ -26,14 +26,21 @@ public class GameServices implements IServerGame {
     public Result createGame(Request request){ //(String authToken, String gameId){
         String authToken = request.getAuthToken();
         String gameId = request.getGameId();
-        Integer commandNum = request.getCommandNum();
+        int commandNum = request.getCommandNum();
         Result result = new Result();
 
         //check if requesting client is an active (logged in) client
         if(Database.getInstance().getClients().contains(authToken))
         {
+            //check if gameId is empty
+            if(gameId.equals(""))
+            {
+                result.setSuccess(false);
+                result.setErrorMsg("Please enter a valid game ID.");
+                System.out.println("ERROR: in createGame() -- Empty gameID");
+            }
             //check if gameId already exists
-            if(!Database.getInstance().getGames().containsKey(gameId))
+            else if(!Database.getInstance().getGames().containsKey(gameId))
             {
                 //if it doesn't exist yet, create it
                 Game newGame = new Game(gameId);
@@ -61,12 +68,14 @@ public class GameServices implements IServerGame {
             {
                 result.setSuccess(false);
                 result.setErrorMsg("The requested game ID already exists.");
+                System.out.println("ERROR: in createGame() -- Duplicate gameID");
             }
         }
         else
         {
             result.setSuccess(false);
             result.setErrorMsg("Invalid authorization token.");
+            System.out.println("ERROR: in createGame() -- Invalid auth token");
         }
         return result;
     }
@@ -76,6 +85,10 @@ public class GameServices implements IServerGame {
         String authToken = request.getAuthToken();
         String gameId = request.getGameId();
         Result result = new Result();
+
+        //TODO: revisit what should happen when attempting to re-join a game
+        //If it goes to a new screen, do that again, basically this would be how to get back to the game screen
+        //if it stays in the lobby screen, print a message saying "you are already in that game"
 
         //check if requesting client is an active (logged in) client
         if(Database.getInstance().getClients().contains(authToken))
@@ -116,19 +129,22 @@ public class GameServices implements IServerGame {
                 else
                 {
                     result.setSuccess(false);
-                    result.setErrorMsg("The requested username is already part of the game.");
+                    result.setErrorMsg("You are already in that game.");
+                    System.out.println("ERROR: in joinGame() -- Requesting user already in requested game");
                 }
             }
             else
             {
                 result.setSuccess(false);
-                result.setErrorMsg("The requested game ID doesn't exist.");
+                result.setErrorMsg("The requested game ID does not exist.");
+                System.out.println("ERROR: in joinGame() -- Game doesn't exist");
             }
         }
         else
         {
             result.setSuccess(false);
             result.setErrorMsg("Invalid authorization token.");
+            System.out.println("ERROR: in joinGame() -- Invalid auth token");
         }
         return result;
     }
@@ -148,19 +164,22 @@ public class GameServices implements IServerGame {
                 Game currentGame = Database.getInstance().getGames().get(gameId);
                 String username = Database.getInstance().getUsername(authToken);
 
-                // Check if player not in a current game
-                if(currentGame.getPlayers().contains(username)) {
-
+                // Check if player is not in the current game
+                if(currentGame.getPlayers().contains(username))
+                {
                     // Check if game is in activeGame list
-                    if(Database.getInstance().getActiveGames() == null)
+                    //if(Database.getInstance().getActiveGames().isEmpty()) //if(Database.getInstance().getActiveGames() == null)
+                    if(!Database.getInstance().getActiveGames().containsKey(gameId))
                     {
                         // Add game to activeGame hashMap
                         Game activeGame = Database.getInstance().getGames().get(gameId);
-                        HashMap<String,Game> temp =  Database.getInstance().getActiveGames();
-                        temp.put(gameId, activeGame);
+                        Database.getInstance().getActiveGames().put(gameId, activeGame);
 
-                        //Add activeGame to database
-                        Database.getInstance().setActiveGames(temp);
+//                        HashMap<String,Game> temp =  Database.getInstance().getActiveGames();
+//                        temp.put(gameId, activeGame);
+//
+//                        //Add activeGame to database
+//                        Database.getInstance().setActiveGames(temp);
 
 
                         //create commands
@@ -175,54 +194,58 @@ public class GameServices implements IServerGame {
 
                         System.out.println(gameId+ " started.");
 
-                    }else if(!Database.getInstance().getActiveGames().containsKey(gameId)){
-
-                        // Add game to activeGame hashMap
-                        Game activeGame = Database.getInstance().getGames().get(gameId);
-                        HashMap<String,Game> temp =  Database.getInstance().getActiveGames();
-                        temp.put(gameId, activeGame);
-
-                        //Add activeGame to database
-                        Database.getInstance().setActiveGames(temp);
-
-
-                        //create commands for other active clients
-                        Request clientRequest = new Request();
-                        //clientRequest.setAuthToken(authToken); //DO THIS FOR EACH METHOD
-                        //clientRequest.setUsername(username); //This is specific to createGame()
-                        clientRequest.setGameId(gameId); //This is specific to startGame()
-                        //add command for other clients
-                        //creates a command object for each client except the requesting client
-                        ClientProxy.getInstance().startGame(clientRequest);
-
-                        result = updateClient(request);
-
-                        System.out.println(gameId+ " started.");
-
-
-                    }else
+                    }
+//                    else if(!Database.getInstance().getActiveGames().containsKey(gameId))
+//                    {
+//                        // Add game to activeGame hashMap
+//                        Game activeGame = Database.getInstance().getGames().get(gameId);
+//                        HashMap<String,Game> temp =  Database.getInstance().getActiveGames();
+//                        temp.put(gameId, activeGame);
+//
+//                        //Add activeGame to database
+//                        Database.getInstance().setActiveGames(temp);
+//
+//
+//                        //create commands for other active clients
+//                        Request clientRequest = new Request();
+//                        //clientRequest.setAuthToken(authToken); //DO THIS FOR EACH METHOD
+//                        //clientRequest.setUsername(username); //This is specific to createGame()
+//                        clientRequest.setGameId(gameId); //This is specific to startGame()
+//                        //add command for other clients
+//                        //creates a command object for each client except the requesting client
+//                        ClientProxy.getInstance().startGame(clientRequest);
+//
+//                        result = updateClient(request);
+//
+//                        System.out.println(gameId+ " started.");
+//                    }
+                    else
                     {
                         result.setSuccess(false);
-                        result.setErrorMsg("The requested gameId is already is already active.");
+                        result.setErrorMsg("That game is already started.");
+                        System.out.println("ERROR: in startGame() -- Game is already started");
                     }
-
                 }
                 else
                 {
                     result.setSuccess(false);
-                    result.setErrorMsg("The requested username is already part of the game.");
+                    result.setErrorMsg("You cannot start that, you are not in that game.");
+                    System.out.println("ERROR: in startGame() -- Requesting user is not in the requested game");
                 }
             }
             else
             {
                 result.setSuccess(false);
                 result.setErrorMsg("The requested game ID doesn't exist.");
+                System.out.println("ERROR: in startGame() -- Game doesn't exist");
             }
         }
         else
         {
             result.setSuccess(false);
             result.setErrorMsg("Invalid authorization token.");
+            System.out.println("ERROR: in startGame() -- Invalid auth token");
+
         }
         return result;
     }
@@ -230,7 +253,7 @@ public class GameServices implements IServerGame {
     @Override //polling response
     public Result updateClient(Request request) { //(String authToken);
         String authToken = request.getAuthToken();
-        Integer commandNum = request.getCommandNum();
+        int commandNum = request.getCommandNum();
        // String gameId = request.getGameId();
         Result result = new Result();
 
