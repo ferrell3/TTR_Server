5de93a75c6a6a52139e52bdbdaae8d75e4d001ae
Kip Hacking <kiphacking@gmail.com>
2018-03-02 14:49:16 -0700

    created chat class and updated Game class

---
+++ server/src/main/java/Models/Chat.java
@@ -0,0 +1,38 @@
+package Models;
+
+/**
+ * Created by kiphacking on 3/2/18.
+ */
+
+public class Chat {
+    private String username;
+    private String message;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+    public String displayChat(){
+
+        return username + ": "+ message;
+
+    }
+
+
+
+
+}
